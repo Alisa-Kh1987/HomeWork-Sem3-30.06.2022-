907acf0423a2b26dd34df3a5e76dc36fe93b2586
Алиса <petrunina.alisa@gmail.com>
2022-07-04 23:39:39 +0300

    Cеминар 3

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//Console.WriteLine("Введите координаты точки по оси Х");
+//int X = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координаты точки по оси Y");
+//int Y = Convert.ToInt32(Console.ReadLine());
+//
+//if(X==0 || Y==0){
+//    Console.WriteLine("Точка находится на оси");
+//}
+//if(X>0 && Y>0){
+ //   Console.WriteLine("Точка находится в первой четверти");
+//}
+//else if(X<0 && Y>0){
+ //   Console.WriteLine("Точка находится во второй четверти");
+//}
+//else if(X<0 && Y<0){
+//    Console.WriteLine("Точка находится в третьей четверти");
+//}
+//else if(X>0 && Y<0){
+//    Console.WriteLine("Точка находится в третьей четверти");
+//}
+
+//static void Main(string[]args{
+    //int[] Input()
+    //{
+     //   int[] Array = new int [2];
+      //  Console.WriteLine("Введите координаты точки по оси Х");
+      //  int X = Convert.ToInt32(Console.ReadLine());
+      //  Array [0] = X;
+      //  Console.WriteLine("Введите координаты точки по оси Y");
+      //  int Y = Convert.ToInt32(Console.ReadLine());
+      //  Array [1] = Y;
+      //  return Array;
+   // }
+//int [] mini = Input ();
+//while (mini [0]==0 || mini[1]==0){
+ //   Console.WriteLine("Точка находится на оси");
+//}
+//if(mini [0]>0 && mini [1] >0){
+    //Console.WriteLine("Точка находится в первой четверти");
+//}
+//else if(mini [0] <0 && mini [1] >0){
+  //  Console.WriteLine("Точка находится во второй четверти");
+//}
+//else if(mini [0] <0 && mini [1] <0){
+//    Console.WriteLine("Точка находится в третьей четверти");
+//}
+//else if(mini [0] >0 && mini [1] <0){
+ //   Console.WriteLine("Точка находится в третьей четверти");
+//}
+//}
